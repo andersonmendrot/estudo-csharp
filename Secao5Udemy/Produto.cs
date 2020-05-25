1142206134b10bb4ba36794dc5f3ca8c463bcfd2
andersonmendrot <anderson.mendrot@gmail.com>
2020-05-25 09:55:27 -0300

    Adicionado Secao 5 até final de palavra this

---
+++ Secao5Udemy/Produto.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Globalization;
+
+namespace Secao5Udemy
+{
+    //toda classe em C# é uma subclasse da classe Object
+    //GetType, Equals, GetHashCode, ToString()
+    class Produto
+    {
+        //atributos começam com letra maiúscula
+        public string Nome;
+        public double Preco;
+        public int Quantidade;
+
+
+        //construtor: utilizado para inicializar valores dos atributos ou para permitir/obrigado que o objeto receba dados no momento de sua instanciação (injeção de dependencias)
+
+        //sobrecarga: é um recurso que uma classe possuo de oferecer mais de uma operação com o mesmo nome, porém com diferentes listas de parâmetros
+
+        //uso de this:
+        //1 - diferenciar atributos de variáveis locais (mais usado em Java)
+        //2 - referenciar outro construtor em um construtor
+        //3 - passar o proprio objeto como argumento na chamada de um método ou construtor
+        
+        //construtor padrão
+        public Produto()
+        {
+            Quantidade = 10;
+        }
+
+        //Referenciar outro construtor em um construtor
+        public Produto(string nome) : this()
+        {
+            Nome = nome;
+        }
+
+        public Produto(string nome, double preco, int quantidade)
+        {
+            Nome = nome;
+            Preco = preco;
+            Quantidade = quantidade;
+        }
+
+        //Quantidade já se inicializa com 0 
+        public Produto(string nome, double preco)
+        {
+            Nome = nome;
+            Preco = preco;
+        }
+
+        ////////////////////////////////////////////////
+
+        // Encapsulamento
+        // Esconder detalhes de implementação de um componente expondo apenas operações seguras e que o mantenha em um estado consistente
+
+
+
+
+
+        ////////////////////////////////////////////////
+
+        //métodos começam com letra maiúscula
+        public double ValorTotalEmEstoque()
+        {
+            return Preco * Quantidade;
+        }
+
+        public void AdicionarProdutos(int qtd)
+        {
+            Quantidade += qtd;
+        }
+
+        //sobreposição de metodo ToString da classe Object
+        //'F2' retorna apenas duas casas decimais do valor double
+        public override string ToString()
+        {
+            return $"{Nome}, ${Preco.ToString("F2", CultureInfo.InvariantCulture)}, ${Quantidade}";
+        }
+    }
+}
---
+++ Secao5Udemy/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Globalization;
+
+namespace Secao5Udemy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nome = Console.ReadLine();
+            double preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            int quantidade = int.Parse(Console.ReadLine());
+
+            Produto p = new Produto(nome, preco, quantidade);
+            Console.WriteLine(p);
+
+            Produto p2 = new Produto(nome, preco);
+
+            Produto p3 = new Produto();
+
+            //Para que esta sintaxe alternativa funcione, o construtor padrão deve estar declarado na classe Produto ou mesmo que não esteja deve ser possível utilizá-lo
+            Produto p4 = new Produto { 
+                Nome = nome,
+                Preco = preco, 
+                Quantidade = quantidade 
+            };
+        }
+    }
+}
